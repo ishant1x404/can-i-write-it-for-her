c5bd846ccef45553f999b0c8ffff33f72b3f347c
Ishant <ishant1x404@gmail.com>
2026-09-03 08:47:09 +0530

    Change music file paths to relative links
    Updated music file paths to be relative.

--- js/data.js
+++ js/data.js
@@ -1,8 +1,8 @@
 const SAKSHI={identity:{name:'Sakshi',nicknames:['Malkin','Bauni'],dob:'02 Oct 2009',height:'5\'5" (165 cm approx.)',metIn:'2025',mbti:'ISFP',dream:'Pilot'},favorites:{colors:['Black','Pink'],food:'Biryani',movies:['The Amazing Spider-Man','The Amazing Spider-Man 2'],manhwa:'Lookism',kdrama:'True Beauty',games:['Roblox','Free Fire'],place:'Japan',subject:'Mathematics',sport:'Volleyball'},hobbies:['Drawing','Dancing','Volleyball'],music:[
-{id:'elvis',artist:'Elvis Presley',title:"Can't Help Falling in Love",src:'assets/music/elvis/cant-help-falling-in-love.mp3',start:80,end:100,art:'assets/images/profile/sakshi.jpg'},
-{id:'mj-human',artist:'Michael Jackson',title:'Human Nature',src:'assets/music/mj/human-nature.mp3',start:48,end:68,art:'assets/images/gallery/michael-jackson.jpg'},
-{id:'mj-beat',artist:'Michael Jackson',title:'Beat It',src:'assets/music/mj/beat-it.mp3',start:68,end:88,art:'assets/images/gallery/michael-jackson.jpg'},
-{id:'mj-thriller',artist:'Michael Jackson',title:'Thriller',src:'assets/music/mj/thriller.mp3',start:59,end:79,art:'assets/images/gallery/michael-jackson.jpg'},
-{id:'mj-billie',artist:'Michael Jackson',title:'Billie Jean',src:'assets/music/mj/billie-jean.mp3',start:50,end:70,art:'assets/images/gallery/michael-jackson.jpg'},
-{id:'x-hope',artist:'XXXTENTACION',title:'HOPE',src:'assets/music/xxxtentacion/hope.mp3',start:42,end:62,art:'assets/images/gallery/xxxtentacion.jpg'},
-{id:'x-changes',artist:'XXXTENTACION',title:'changes',src:'assets/music/xxxtentacion/changes.mp3',start:43,end:63,art:'assets/images/gallery/xxxtentacion.jpg'}]};
+{id:'elvis',artist:'Elvis Presley',title:"Can't Help Falling in Love",src:'./assets/music/elvis/cant-help-falling-in-love.mp3',start:80,end:100,art:'assets/images/profile/sakshi.jpg'},
+{id:'mj-human',artist:'Michael Jackson',title:'Human Nature',src:'./assets/music/mj/human-nature.mp3',start:48,end:68,art:'assets/images/gallery/michael-jackson.jpg'},
+{id:'mj-beat',artist:'Michael Jackson',title:'Beat It',src:'./assets/music/mj/beat-it.mp3',start:68,end:88,art:'assets/images/gallery/michael-jackson.jpg'},
+{id:'mj-thriller',artist:'Michael Jackson',title:'Thriller',src:'./assets/music/mj/thriller.mp3',start:59,end:79,art:'assets/images/gallery/michael-jackson.jpg'},
+{id:'mj-billie',artist:'Michael Jackson',title:'Billie Jean',src:'./assets/music/mj/billie-jean.mp3',start:50,end:70,art:'assets/images/gallery/michael-jackson.jpg'},
+{id:'x-hope',artist:'XXXTENTACION',title:'HOPE',src:'./assets/music/xxxtentacion/hope.mp3',start:42,end:62,art:'assets/images/gallery/xxxtentacion.jpg'},
+{id:'x-changes',artist:'XXXTENTACION',title:'changes',src:'./assets/music/xxxtentacion/changes.mp3',start:43,end:63,art:'assets/images/gallery/xxxtentacion.jpg'}]};
